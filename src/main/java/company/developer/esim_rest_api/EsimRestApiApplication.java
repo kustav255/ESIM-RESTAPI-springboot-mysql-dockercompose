255e88dc8b628e38bdbef057ebf556cdbc9fed28
kustav255 <grmguga10@gmail.com>
2025-08-14 01:42:11 -0300

    setting up the project with spring initializr

---
+++ src/main/java/company/developer/esim_rest_api/EsimRestApiApplication.java
@@ -0,0 +1,13 @@
+package company.developer.esim_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsimRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsimRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/company/developer/esim_rest_api/EsimRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package company.developer.esim_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsimRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
