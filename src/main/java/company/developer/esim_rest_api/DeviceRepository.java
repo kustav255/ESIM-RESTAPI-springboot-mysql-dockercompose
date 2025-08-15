b48b1504831e6ab34dfbcbbef69e75456b510733
kustav255 <grmguga10@gmail.com>
2025-08-14 22:31:24 -0300

    added simple test cases with mock data

--- src/main/java/company/developer/esim_rest_api/DeviceRepository.java
+++ src/main/java/company/developer/esim_rest_api/DeviceRepository.java
@@ -1,7 +1,9 @@
 package company.developer.esim_rest_api;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeviceRepository extends CrudRepository<Device, Integer> {
 
     Iterable<Device> findByBrandContainingIgnoreCase(String brand);
